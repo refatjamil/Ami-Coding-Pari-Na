661880981b4b02debcac6714274384c672d97680
Rifat Jamil <rifatjamil54@gmail.com>
2023-08-13 11:24:02 +0600

    Section 2 - Added 'KhojAdmin' in admin.py. - Modify views.py 	- added docstring in every function 	- implemented logic for stored descending order input_values in database. 	- FIX: ValueError by try except - Modify in templates.

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Khoj
 # Register your models here.
+@admin.register(Khoj)
+class KhojAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_id', 'input_values', 'timestamp']
--- app/views.py
+++ app/views.py
@@ -5,17 +5,22 @@ from .forms import RegistrationForm, LoginForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from .models import Khoj
+from django.contrib import messages
 
 def welcome(request):
+    """ Welcome to user """
     return render(request, 'welcome.html')
 
 def user_registration(request):
+    """ Register a new user account. """
+
     if request.method == "POST":
         registration_form = RegistrationForm(request.POST)
         if registration_form.is_valid():
             messages.success(request, 'Congratulation!! You have become a Author. Please Login.')
             registration_form.save()
+            return redirect('login')
     else:    
         registration_form = RegistrationForm()
 
@@ -23,6 +28,8 @@ def user_registration(request):
     return render(request, 'registration.html', context)
 
 def user_login(request):
+    """  Authenticate a user and log them in. """
+
     if not request.user.is_authenticated:
         if request.method == 'POST':
             login_form = LoginForm(request=request, data=request.POST)
@@ -44,18 +51,29 @@ def user_login(request):
         return redirect('/')
     
 def user_logout(request):
+    """ Log out the currently authenticated user. """
+
     logout(request)
     return redirect('/')  
 
-def khoj_result(input_values, search_value):
-    list_of_values = [int(i) for i in input_values.split(',')]
 
-    if int(search_value) in list_of_values:
-        return True
-    else:
-        return False
+
 
 class KhojView(LoginRequiredMixin, View):
+    """
+    This class-based view, `KhojView`, provides functionality for searching an input list of integers for a specific value.
+
+    When accessed via a GET request, the view renders a form (`KhojForm`) to input a list of integers and a search value.
+
+    Upon receiving a POST request with valid form data:
+    - It extracts the input values and the search value from the form.
+    - It processes the input values, checking if the search value is present in the list.
+    - If the search value is found, `khoj_result` is set to True; otherwise, it's set to False.
+    - The input values are then sorted in descending order and stored as a string in the database along with the user ID.
+    - Finally, the rendered HTML page is updated with the search result (`khoj_result`).
+
+    """
+
     def get(self, request, *args, **kwargs):
         khoj_form = KhojForm
         context = {'khoj_form':khoj_form}
@@ -67,5 +85,22 @@ class KhojView(LoginRequiredMixin, View):
             input_values = khoj_form.cleaned_data['input_values']
             search_value = khoj_form.cleaned_data['search_value']
 
-        context = {'khoj_result': khoj_result(input_values, search_value)}    
+            list_of_values = []
+            for i in input_values.split(','):
+                try:
+                    integer = int(i)
+                    list_of_values.append(integer)
+                    if int(search_value) in list_of_values:
+                        khoj_result = True
+                    else:
+                        khoj_result = False
+                except ValueError:
+                    messages.error(request, "Please enter a valid integer.")
+                    return redirect('khoj')
+
+            desorted_input_values = sorted(list_of_values, reverse=True) # sorted 'list_of_values' in descending order
+            sorted_string_input_values = ", ".join([str(num) for num in desorted_input_values]) # desorted_input_values >>> type:list  to type:str >>> sorted_string_input_values
+            Khoj(user_id=request.user, input_values=sorted_string_input_values).save() # 'sorted_string_input_values' store in database as string
+
+        context = {'khoj_result': khoj_result}    
         return render(request, 'khoj.html', context)
