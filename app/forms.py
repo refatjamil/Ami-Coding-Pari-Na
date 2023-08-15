0097d9188710a12ae69cbe5ec3328f7b7661e1d8
Rifat Jamil <rifatjamil54@gmail.com>
2023-08-15 10:12:52 +0600

    Font-End Work - Config static - Used bootstrap - Modify templates

--- app/forms.py
+++ app/forms.py
@@ -6,29 +6,32 @@ from django.utils.translation import gettext, gettext_lazy as _
 
 
 class KhojForm(forms.ModelForm):
-    search_value = forms.CharField(max_length=50, required=False)  # Adding the extra field
+    search_value = forms.CharField(max_length=50, required=False, widget=forms.TextInput(attrs={'autofocus':True, 'class':'form-control Roboto'}))  # Adding the extra field
 
     class Meta:
         model = Khoj
         fields = ['input_values']
+        widgets = {
+            'input_values': forms.TextInput(attrs={'class':'form-control Roboto'}),
+             }
    
 
 class RegistrationForm(UserCreationForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}),)
-    password2 = forms.CharField(label='Confirm Password (again)', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control Roboto'}),)
+    password2 = forms.CharField(label='Confirm Password (again)', widget=forms.PasswordInput(attrs={'class':'form-control Roboto'}))
     
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
         labels = {'first_name': 'First Name', 'last_name': 'Last Name'}
         widgets = {
-            'username': forms.TextInput(attrs={'class':'form-control'}),
-            'first_name': forms.TextInput(attrs={'class':'form-control'}),
-            'last_name': forms.TextInput(attrs={'class':'form-control'}),
-            'email': forms.EmailInput(attrs={'class':'form-control'}),
+            'username': forms.TextInput(attrs={'class':'form-control Roboto'}),
+            'first_name': forms.TextInput(attrs={'class':'form-control Roboto'}),
+            'last_name': forms.TextInput(attrs={'class':'form-control Roboto'}),
+            'email': forms.EmailInput(attrs={'class':'form-control Roboto'}),
 
-            }   
+             }  
         
 class LoginForm(AuthenticationForm):
-    username = UsernameField(widget=forms.TextInput(attrs={'autofocus':True, 'class':'form-control'}))
-    password = forms.CharField(label=_("Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete': "current-password", 'class':'form-control'}))        
+    username = UsernameField(widget=forms.TextInput(attrs={'autofocus':True, 'class':'form-control Roboto'}))
+    password = forms.CharField(label=_("Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete': "current-password", 'class':'form-control Roboto'}))        
